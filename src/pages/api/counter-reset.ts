20ad5195274c495f1bb5f2c0e705af7d329c07c2
Umut Toker <utoker@gmail.com>
2023-01-18 09:36:19 -0500

    fix cron

--- src/pages/api/counter-reset.ts
+++ src/pages/api/counter-reset.ts
@@ -5,6 +5,10 @@ import { prisma } from '../../server/db/client';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const auth = req.headers.authorization;
+  if (req.method !== 'POST') {
+    res.status(405).json('Method not allowed');
+    return;
+  }
   if (!auth || auth !== env.API_ROUTE_SECRET) {
     res.status(401).json('Unauthorized');
     return;
