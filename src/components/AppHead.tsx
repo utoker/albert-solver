494d25b407bfd10e9d411d8547c89c3e0fef8b63
Umut Toker <utoker@gmail.com>
2023-01-22 22:05:48 -0500

    preload

--- src/components/AppHead.tsx
+++ src/components/AppHead.tsx
@@ -9,6 +9,12 @@ const AppHead = () => {
         name="description"
         content="Get expert help with your assessments and improve your grades with our AI-powered website. Ask our AI any question and receive accurate and reliable answers quickly. Perfect for students of all levels looking to excel in their studies."
       />
+      <link
+        rel="preload"
+        href="/api/stripe/get-prices"
+        as="fetch"
+        crossOrigin="anonymous"
+      ></link>
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
