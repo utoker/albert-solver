40db5efebcde30e2a427382263719ddf19635728
Umut Toker <utoker@gmail.com>
2023-04-08 12:06:35 -0400

    fix

--- src/pages/api/openai/generate.ts
+++ src/pages/api/openai/generate.ts
@@ -7,28 +7,29 @@ export const config = {
 };
 
 const handler = async (req: NextRequest): Promise<Response> => {
-  const { prompt } = (await req.json()) as {
-    prompt?: string;
-  };
+  try {
+    const { prompt } = (await req.json()) as {
+      prompt?: string;
+    };
 
-  if (!prompt) {
-    return new Response('No prompt in the request', { status: 400 });
+    if (!prompt) {
+      return new Response('No prompt in the request', { status: 400 });
+    }
+    console.log(prompt);
+    const payload: OpenAIStreamPayload = {
+      model: 'text-davinci-003',
+      prompt: `The following is a conversation with an AI assistant named Albert Solver. The assistant is helpful, creative, clever, and very friendly. The assistant answers in the language of the question. \n${prompt}`,
+      temperature: 0.7,
+      max_tokens: 2048,
+      stream: true,
+    };
+
+    const stream = await OpenAIStream(payload);
+    return new Response(stream);
+  } catch (error) {
+    console.error(error);
+    return new Response('Error', { status: 500 });
   }
-
-  const payload: OpenAIStreamPayload = {
-    model: 'gpt-3.5-turbo',
-    prompt: `The following is a conversation with an AI assistant named Albert Solver. The assistant is helpful, creative, clever, and very friendly. The assistant answers in the language of the question. \n${prompt}`,
-    temperature: 0.7,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    max_tokens: 2048,
-    stream: true,
-    n: 1,
-  };
-
-  const stream = await OpenAIStream(payload);
-  return new Response(stream);
 };
 
 export default handler;
