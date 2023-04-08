4743d32e83ede39ed7db1cd0d2b0abf8be4866ba
Umut Toker <utoker@gmail.com>
2023-04-08 12:15:38 -0400

    fix

--- src/utils/OpenAIStream.ts
+++ src/utils/OpenAIStream.ts
@@ -5,12 +5,8 @@ export interface OpenAIStreamPayload {
   model: string;
   prompt: string;
   temperature: number;
-  top_p: number;
-  frequency_penalty: number;
-  presence_penalty: number;
   max_tokens: number;
   stream: boolean;
-  n: number;
 }
 
 export async function OpenAIStream(payload: OpenAIStreamPayload) {
